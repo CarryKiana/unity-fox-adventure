227811dd6086f36e74d89322a31a61ee7a19e489
yukairui <825294867@qq.com>
2022-08-11 16:59:35 +0800

    敌人移动

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,6 +13,7 @@ public class PlayerController : MonoBehaviour
     public Collider2D coll;
     public int Cherry;
     public Text CherryNum;
+    private bool isHurt;
 
     // Start is called before the first frame update
     void Start()
@@ -24,7 +25,9 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        Movement();
+        if (!isHurt) {
+            Movement();
+        }
         SwitchAnim();
     }
 
@@ -58,6 +61,14 @@ public class PlayerController : MonoBehaviour
                 anim.SetBool("jumping", false);
                 anim.SetBool("falling", true);
             }
+        } else if (isHurt) {
+            anim.SetBool("hurt", true);
+            anim.SetFloat("running", 0);
+            if (Mathf.Abs(rb.velocity.x) < 0.1f) {
+                anim.SetBool("hurt", false);
+                anim.SetBool("idle", true);
+                isHurt = false;
+            }
         } else if (coll.IsTouchingLayers(ground)) {
             anim.SetBool("falling", false);
             anim.SetBool("idle", true);
@@ -73,13 +84,20 @@ public class PlayerController : MonoBehaviour
     }
 
     private void OnCollisionEnter2D(Collision2D collision) {
-        if (anim.GetBool("falling")) {
-            if (collision.gameObject.tag == "Enemy") {
+        if (collision.gameObject.tag == "Enemy") {
+            if (anim.GetBool("falling")) {
                 Destroy(collision.gameObject);
                 rb.velocity = new Vector2(rb.velocity.x, jumpforce * Time.deltaTime);
                 anim.SetBool("jumping", true);
+            } else if (transform.position.x < collision.gameObject.transform.position.x) {
+                // 左侧
+                isHurt = true;
+                rb.velocity = new Vector2(-5, rb.velocity.y);
+            } else if (transform.position.x > collision.gameObject.transform.position.x) {
+                // 右侧
+                isHurt = true;
+                rb.velocity = new Vector2(5, rb.velocity.y);
             }
         }
-
     }
 }
